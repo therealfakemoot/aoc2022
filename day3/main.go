93a6e30ea6aed7ceb908af8e179d1996d6d5261a
Nick Dumas <dumasn@homes.com>
2022-12-15 11:14:07 -0500

    Trying to make my pipeline homomorphic.
    []string slices are how my commonCharacters() func returns its set of
    data, and by accepting that as an input, it'll be easier to
    progressively chain commonality checks for part 2.

--- day3/main.go
+++ day3/main.go
@@ -8,13 +8,33 @@ import (
 	"text/scanner"
 )
 
-func splitString(s string) (ret [2]string) {
+func priority(s string) int {
+	for i, p := range priorities {
+		if p == s {
+			return i + 1
+		}
+	}
+
+	return 0
+}
+
+var priorities = [...]string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"}
+
+func splitRecord(s []string) (ret [2][]string) {
 	ret[0], ret[1] = s[:len(s)/2], s[len(s)/2:]
 	return
 }
 
-func LoadRecords(fn string) ([]string, error) {
-	records := make([]string, 0)
+func explodeString(s string) []string {
+	ret := make([]string, 0)
+	for _, r := range s {
+		ret = append(ret, string(r))
+	}
+	return ret
+}
+
+func LoadRecords(fn string) ([][]string, error) {
+	records := make([][]string, 0)
 	f, err := os.Open(fn)
 	if err != nil {
 		return records, fmt.Errorf("error opening inventory source file: %w", err)
@@ -22,22 +42,23 @@ func LoadRecords(fn string) ([]string, error) {
 	var s scanner.Scanner
 	s.Init(f)
 	for tok := s.Scan(); tok != scanner.EOF; tok = s.Scan() {
-		records = append(records, s.TokenText())
+		records = append(records, explodeString(s.TokenText()))
 		// fmt.Printf("%s: %s\n", s.Position, s.TokenText())
 	}
 	return records, nil
 }
 
-func commonCharacters(pairs [2]string) []string {
+func commonCharacters(pairs [2][]string) []string {
 	ret := make(map[string]bool)
-	left, right := pairs[0], pairs[1]
-	for _, leftItem := range left {
-		for _, rightItem := range right {
-			if leftItem == rightItem {
-				ret[string(leftItem)] = true
+
+	for _, cLeft := range pairs[0] {
+		for _, cRight := range pairs[1] {
+			if cLeft == cRight {
+				ret[cLeft] = true
 			}
 		}
 	}
+
 	dedupe := make([]string, 0)
 	for k := range ret {
 		dedupe = append(dedupe, k)
@@ -45,6 +66,17 @@ func commonCharacters(pairs [2]string) []string {
 	return dedupe
 }
 
+func Part1(records [][]string) {
+	sum := 0
+	for _, record := range records {
+		split := splitRecord(record)
+		commonChars := commonCharacters(split)
+		log.Printf("%#+v\n", commonChars)
+		p := priority(commonChars[0])
+		sum += p
+	}
+	log.Printf("Priority sum: %d\n", sum)
+}
 func main() {
 	var (
 		filename string
@@ -58,9 +90,6 @@ func main() {
 		log.Fatalf("LoadRecords failed: %s\n", err)
 	}
 
-	for _, record := range records {
-		split := splitString(record)
-		log.Println(commonCharacters(split))
-	}
+	Part1(records)
 
 }
