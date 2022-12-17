3e0427e6c2f1cc3d0cd155551beff7a0f89f96b1
Nick Dumas <dumasn@homes.com>
2022-12-16 20:55:54 -0500

    uh oh

--- day4/main.go
+++ day4/main.go
@@ -14,8 +14,16 @@ type Span struct {
 	Start, Stop int
 }
 
-func (s Span) Overlap(other Span) bool {
-	if s.Stop <= other.Stop && other.Start < s.Stop {
+func (s Span) Overlaps(other Span) bool {
+	if s.Stop < other.Stop && other.Start < s.Stop {
+		return true
+	}
+	return false
+}
+
+func (s Span) Contains(other Span) bool {
+	//if s.Stop <= other.Stop && other.Start < s.Stop {
+	if other.Start >= s.Start && other.Stop <= s.Stop {
 		return true
 	}
 	return false
@@ -52,12 +60,23 @@ func LoadRecords(fn string) ([][2]Span, error) {
 func Part1(records [][2]Span) {
 	sum := 0
 	for _, r := range records {
-		if r[0].Overlap(r[1]) || r[1].Overlap(r[0]) {
-			log.Printf("overlap found: %#v\n", r)
+		if r[0].Contains(r[1]) || r[1].Contains(r[0]) {
 			sum++
 		}
 	}
 	fmt.Printf("Part 1 sum: %d\n", sum)
+}
+
+func Part2(records [][2]Span) {
+	sum := 0
+	for _, pair := range chunkBy(records, 2) {
+		//if r[0].Overlaps(r[1]) || r[1].Overlaps(r[0]) {
+		a, b := pair[0], pair[1]
+		if (a[0].Overlaps(a[1]) || (a[1].Overlaps(a[0]))) && (b[0].Overlaps(b[1]) || (b[1].Overlaps(b[0]))) {
+			sum++
+		}
+	}
+	fmt.Printf("Part 2 sum: %d\n", sum)
 }
 
 func main() {
@@ -73,5 +92,13 @@ func main() {
 		log.Fatalf("error loading records: %s\n", err)
 	}
 	Part1(records)
+	Part2(records)
 
+}
+
+func chunkBy[T any](items []T, chunkSize int) (chunks [][]T) {
+	for chunkSize < len(items) {
+		items, chunks = items[chunkSize:], append(chunks, items[0:chunkSize:chunkSize])
+	}
+	return append(chunks, items)
 }
